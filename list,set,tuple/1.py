d576e1107bf3f301db5b9b8e149c64fb713b213a
Urvansh-eng <kken11350@gmail.com>
2026-06-06 14:47:51 +0530

    Learned list,set,tuple

---
+++ list,set,tuple/1.py
@@ -0,0 +1,40 @@
+# collection = single "variable" used to store multiple values
+
+# List  = [] ordered and changeable. Duplicates OK
+# Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+# Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+# 
+# fruits = ["apple","orange","banana","lichi"]
+
+# print(fruits[::-1])
+
+
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0,"pineapple")
+# fruits.sort()
+# for fruit in fruits:
+#     print(fruit)
+
+# print("apple" in fruits)
+
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("apple"))
+# print(fruits)
+
+
+# set
+fruits = {"apple","orange","banana","lichi"}
+
+
+# print(dir(fruits))
+# print(len(fruits))
+# print(help(fruits))
+# print("apple" in fruits) 
+# fruits.add("pineapple")
+# fruits.remove("apple")
+fruits.clear()
+print(fruits)
